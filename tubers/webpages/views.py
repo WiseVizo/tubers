8f55f4a35d5515588af11381692d35aaae2365d0
WiseVizo <mecoc1011@gmail.com>
2024-03-24 12:22:40 +0530

    synced latest tubers frontend with backend data

--- tubers/webpages/views.py
+++ tubers/webpages/views.py
@@ -7,10 +7,12 @@ def home(request):
     sliders = Slider.objects.all()
     our_team = OurTeam.objects.all()
     featured_tubers = Youtuber.objects.order_by("-created_at").filter(is_featured=True)
+    all_tubers = Youtuber.objects.all()
     data =  {
         "sliders": sliders, 
         "our_team": our_team,
         "featured_tubers": featured_tubers,
+        "youtubers": all_tubers,
     }
     return render(request, os.path.join("webpages", "home.html"), data)
 
