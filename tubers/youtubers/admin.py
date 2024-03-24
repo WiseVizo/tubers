b8c1b30402817b0402ebae205306afe530301991
WiseVizo <mecoc1011@gmail.com>
2024-03-24 10:19:44 +0530

    created youtubers model

--- tubers/youtubers/admin.py
+++ tubers/youtubers/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import *
 
+
+class YtAdmin(admin.ModelAdmin):
+    list_display = ('id','first_name', 'last_name', 'sub_count', 'is_featured')
+    list_filter = ('camera_type', 'city')
+    list_display_links = ('first_name', 'last_name')
+    ordering = ('id',)
+
 # Register your models here.
-admin.site.register(Youtuber)
+admin.site.register(Youtuber, YtAdmin)
--- tubers/youtubers/models.py
+++ tubers/youtubers/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+
+
 # Create your models here.
 class Youtuber(models.Model):
     CREW_CHOICES = (
