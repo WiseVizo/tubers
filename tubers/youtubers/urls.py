6b2263c2d62f4c6b74730781c609165a79aa092c
WiseVizo <mecoc1011@gmail.com>
2024-03-27 10:05:53 +0530

    setting up the tubers page

--- tubers/youtubers/urls.py
+++ tubers/youtubers/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.youtubers, name="youtubers"),
-    path("<int:id>", views.youtuber_details, name="youtuber_detalis"),
+    path("<int:id>", views.youtuber_details, name="youtuber_details"),
     path("search", views.search, name="search"),
 ]
--- tubers/youtubers/views.py
+++ tubers/youtubers/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from .models import Youtuber
 # Create your views here.
 def youtubers(request):
-    pass
+    tubers = Youtuber.objects.all()
+    data = {
+        'tubers' : tubers,
+    }
+    return render(request, "youtubers/youtubers.html", data)
 
 def youtuber_details(request, id):
     pass
