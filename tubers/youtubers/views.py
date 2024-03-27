02fdbd7c0fd842dfa4e2dcd83b8217c42b5ce9ee
WiseVizo <mecoc1011@gmail.com>
2024-03-27 12:00:03 +0530

    tubers_detail page

--- tubers/youtubers/views.py
+++ tubers/youtubers/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Youtuber
 # Create your views here.
 def youtubers(request):
@@ -9,7 +9,11 @@ def youtubers(request):
     return render(request, "youtubers/youtubers.html", data)
 
 def youtuber_details(request, id):
-    pass
+    tuber = get_object_or_404(Youtuber, pk=id)
+    data = {
+        'tuber': tuber,
+    }
+    return render(request, "youtubers/youtuber_details.html", data)
 
 def search(request):
     pass
