3991bac13553509e58660ed82aeb9bbe0b58da0e
WiseVizo <mecoc1011@gmail.com>
2024-03-29 10:00:12 +0530

    keyword based search implemented

--- tubers/youtubers/views.py
+++ tubers/youtubers/views.py
@@ -21,7 +21,28 @@ def search(request):
         keyword = request.GET['keyword']
         tubers = tubers.filter(description__icontains=keyword)
 
+    # Get all unique city values from the Youtuber model
+    cities = Youtuber.objects.values_list('city', flat=True).distinct()
+    camera_types = Youtuber.objects.values_list('camera_type', flat=True).distinct()
+    categories = Youtuber.objects.values_list('category', flat=True).distinct()
+
+     # Get the selected values from the request GET parameters
+    selected_city = request.GET.get('city')
+    selected_category = request.GET.get('category')
+    selected_camera_type = request.GET.get('camera_type')
+
+
+
+    if selected_city:
+        tubers = tubers.filter(city=selected_city)
+    if selected_category:
+        tubers = tubers.filter(category=selected_category)
+    if selected_camera_type:
+        tubers = tubers.filter(camera_type=selected_camera_type)
     data = {
         'tubers' : tubers,
+        'cities': cities,
+        'camera_types': camera_types,
+        'categories': categories,
     }
     return render(request, "youtubers/search.html", data)
